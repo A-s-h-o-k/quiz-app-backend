d34fdba736b50b5cbc8ed9d371b3b34fb3f1378f
shivakumar-ch <shivakumar.chelloju@CaprusIT.local>
2022-02-22 11:47:55 +0530

    small changes

--- model/properties.ts
+++ model/properties.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-const props = {
-  DB_URL: "mongodb://localhost:27017/quiz",
-};
-
-module.exports = props;
-=======
 const properties = {
   DB_URL: "mongodb://localhost:27017/quiz",
 };
 
 module.exports = properties;
->>>>>>> 889cc1e55f974b9ede880c334d8bc2db1bacd0b4
--- routes/loginRoute.ts
+++ routes/loginRoute.ts
@@ -1,16 +1,15 @@
 const router = require("express").Router();
 const statModels = require("../model/statisticsSchema.js");
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   console.log(req.body);
   statModels.find(function (err, response) {
     if (err) console.log(err);
-    else res.send({ nothing: "yess" });
+    else res.send(response);
   });
-  res.send({ nothing: "yess" });
 });
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   console.log("--------");
   statModels.find({ name: "shiva" }, function (err, re) {
     if (err) {
